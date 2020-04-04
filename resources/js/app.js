ad3a473eb0e0221f34691347de36a49341a31f30
Antonio Gil <antonioluisgil@gmail.com>
2020-04-04 22:08:24 +0200

    Setup Laravel Sanctum

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,8 +7,9 @@ const files = require.context('./', true, /\.vue$/i);
 files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
 // Axios configuration
-axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.axios = axios;
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+axios.defaults.withCredentials = true;
 
 const app = new Vue({
     el: '#app',
--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,23 +1,26 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
-
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+  <div class="container">
+    {{ user }}
+  </div>
 </template>
 
 <script>
-    export default {
-        mounted() {
-            console.log('Component mounted.')
-        }
+export default {
+  data() {
+    return {
+      user: null
     }
+  },
+
+  mounted() {
+    this.getUser();
+  },
+
+  methods: {
+    async getUser() {
+      const response = await axios.get('api/user');
+      this.user = response.data;
+    }
+  }
+};
 </script>
