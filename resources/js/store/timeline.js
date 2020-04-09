33cdb0dd5c3ea72836c50c7240d834ab4e5a761d
Antonio Gil <antonioluisgil@gmail.com>
2020-04-09 10:49:21 +0200

    Push only tweets are not in the state

--- resources/js/store/timeline.js
+++ resources/js/store/timeline.js
@@ -2,26 +2,28 @@ export default {
   namespaced: true,
 
   state: {
-    tweets: []
+    tweets: [],
   },
 
   getters: {
     tweets(state) {
       return state.tweets.sort((a, b) => b.created_at - a.created_at);
-    }
+    },
   },
 
   mutations: {
     pushTweets(state, tweets) {
-      state.tweets.push(...tweets);
-    }
+      const tweetIds = state.tweets.map((tweet) => tweet.id);
+      const newTweets = tweets.filter((tweet) => tweetIds.includes(tweet.id));
+      state.tweets.push(...newTweets);
+    },
   },
 
   actions: {
     async getTweets({ commit }, { page }) {
       const { data } = await axios.get(`/api/timeline?page=${page}`);
-      commit('pushTweets', data.data);
+      commit("pushTweets", data.data);
       return data;
-    }
-  }
-}
+    },
+  },
+};
